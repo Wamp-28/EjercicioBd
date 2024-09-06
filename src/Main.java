6a8e8cd4d6384a5d0013d11df0ee361e069ad9de
William Alexander Matallana <wmatallana28@gmail.com>
2024-09-06 16:44:06 -0500

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        double numero1, numero2, suma, resta, multi;
+
+        System.out.println("INGRESE EL PRIMER VALOR");
+        numero1 = teclado.nextDouble();
+
+        System.out.println("INGRESE EL SEGUNDO VALOR");
+        numero2 = teclado.nextDouble();
+
+        suma = numero1 + numero2;
+
+        resta = numero1 - numero2;
+
+        multi = numero1 * numero2;
+
+        System.out.println("LA SUMA ES: " + suma);
+        System.out.println("LA RESTA ES: " + resta);
+        System.out.println("LA MULTIPLICACION ES: " + multi);
+
+
+
+
+
+
+
+
+    }
+}
